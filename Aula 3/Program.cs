b8c1a90020eb90e5ee984a045f2c60b64b0acdb7
Ricardo Augusto Vicentini <ric_vicentini@hotmail.com>
2021-10-31 07:45:16 -0300

    Adicionada aula 4

---
+++ Aula 3/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+Pessoa p1 = new Pessoa();
+
+p1.Nome = "Ricardo";
+p1.Idade = 30;
+
+p1.EnderecoPessoa = new Endereco()
+{
+  Logradouro = "Rua teste",
+  Numero = 300,
+  CEP = "000000",
+  Cidade = "São Paulo",
+};
+
+WriteLine("Fim");
---
+++ Aula 4/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+Numero a = new Numero(2);
+Numero b = new Numero(2);
+
+if(a == b)
+{
+  WriteLine($"a e b são iguais");
+}
+else
+{
+  WriteLine("a e b são diferentes");
+}
+
+class Numero
+{
+    public int N { get; set; }
+    public Numero(int n)
+    {
+        N = n;
+    }
+}
